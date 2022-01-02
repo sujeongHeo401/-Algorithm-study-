f506eaa599428779c22f97e9bb3d47f5ca6576fc
sujeongHeo401 <gotnwjd40@gmail.com>
2022-01-02 21:39:04 +0900

    746 번

---
+++ 746_min_cost_start_climbing_stairs/sol.py
@@ -0,0 +1,18 @@
+from typing import List 
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        @cache
+        def minimum_cost(i):
+            if i <= 1:
+                return 0
+            # if i in memo:
+            #     return memo[i]
+
+            down_one= cost[i-1] + minimum_cost(i-1)
+            down_two= cost[i-2] + minimum_cost(i-2)
+            return min(down_one, down_two)
+        # memo = {}
+        return minimum_cost(len(cost))
+
+p = Solution()
+print(p.minCostClimbingStairs([1,100,1,1,1,100,1,1,100,1]))
