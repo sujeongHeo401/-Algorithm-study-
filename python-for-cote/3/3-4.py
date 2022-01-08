51972565fa8cf813b8edf7a532fa62f9d6457612
sujeongHeo401 <gotnwjd40@gmail.com>
2022-01-08 11:52:19 +0900

    return 추가

--- python-for-cote/3/3-4.py
+++ python-for-cote/3/3-4.py
@@ -5,7 +5,12 @@ def sol(N: int, K: int, ans: int) -> int:
     while N > 1:
         N //= K
         ans += 1
+    return ans
 
-sol(25, 5, 0)
-sol(17, 4, ans=0)
+# sol(25, 5, 0)
+# sol(17, 4, ans=0)
+
+# print(sol(25, 5, 0))
+# print(sol(17, 4, ans=0))
+print(sol(20, 3, ans=0))
 
