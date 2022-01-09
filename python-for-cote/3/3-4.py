42dc9340876e14c3603b27b750268a71c9fe1592
sujeongHeo401 <gotnwjd40@gmail.com>
2022-01-09 15:23:43 +0900

    leetcode 2135 Count Words Obtained After Adding a Letter

--- python-for-cote/3/3-4.py
+++ python-for-cote/3/3-4.py
@@ -1,11 +1,19 @@
 def sol(N: int, K: int, ans: int) -> int:
-    if N % K != 0:
-        ans += (N % K)
-        N = N - (N % K)
-    while N > 1:
-        N //= K
-        ans += 1
-    return ans
+    result = 0
+    target = (N // K) * K
+    result += N - target
+    N = target
+    if N < K:
+        break
+    result += N -1
+    # if N % K != 0:
+    #     ans += (N % K)
+    #     N = N - (N % K)
+    # while N > 1:
+    #     N //= K
+    #     ans += 1
+    # print("ans:", ans)
+    # return ans
 
 # sol(25, 5, 0)
 # sol(17, 4, ans=0)
