6539b0373b2ced1fb7994c0b8efff5cfae3f8502
sujeongHeo401 <gotnwjd40@gmail.com>
2022-01-12 04:39:19 +0900

    3-4.. 갓직히 모르겠다

--- python-for-cote/3/3-4.py
+++ python-for-cote/3/3-4.py
@@ -1,24 +1,19 @@
-def sol(N: int, K: int, ans: int) -> int:
-    result = 0
-    target = (N // K) * K
-    result += N - target
-    N = target
-    if N < K:
+n, k = map(int, input().split())
+result = 0
+
+#n이 k 이상이라면 k 로 계속 나누기
+while True:
+    # (N == K로 나누어 떨어지는 수가 될 때 까지 1씩 빼기
+    target = (n // k) * k
+    result += (n - target)
+    n = target
+    # n이 k보다 작을 때 (더 이상 나눌 수 없을 때) 반복문 탈출
+    if n < k:
         break
-    result += N -1
-    # if N % K != 0:
-    #     ans += (N % K)
-    #     N = N - (N % K)
-    # while N > 1:
-    #     N //= K
-    #     ans += 1
-    # print("ans:", ans)
-    # return ans
+    #k로 나누기 
+    result += 1
+    n //= k
 
-# sol(25, 5, 0)
-# sol(17, 4, ans=0)
-
-# print(sol(25, 5, 0))
-# print(sol(17, 4, ans=0))
-print(sol(20, 3, ans=0))
-
+#마지막으로 남은 수에 대하여 1씩 빼기
+result += (n - 1)
+print(result)
