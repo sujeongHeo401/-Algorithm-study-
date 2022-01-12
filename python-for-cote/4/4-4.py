c6a3b33168084c84b24089f38a5159d35d941095
sujeongHeo401 <gotnwjd40@gmail.com>
2022-01-12 13:25:49 +0900

    게임 개발 수정

--- python-for-cote/4/4-4.py
+++ python-for-cote/4/4-4.py
@@ -1,4 +1,7 @@
 # n, m 을 공백으로 구분하여 입력받기
+from typing import NoReturn
+
+
 n, m = map(int, input().split())
 
 #방문한 위치를 저장히기 위한 맵을 생성하여 0으로 초기화
@@ -23,36 +26,33 @@ def turn_left():
     if direction == -1:
         direction = 3
 
-    # 시뮬레이션 시작 
-    count = 1
-    turn_time = 0
-    while True:
-        #왼쪽으로 회전
-        turn_left()
-        nx = x + dx[direction]
-        ny = y + dy[direction]
-
-        if d[nx][ny] == 0 and array[nx][ny] == 0:
-            d[nx][ny] = 1
+# 시뮬레이션 시작 
+count = 1
+turn_time = 0
+while True:
+    turn_left()
+    nx = x + dx[direction]
+    ny = y + dy[direction]
+    # 회전한 이후 정면에 가보지 않은 칸이 존재하는 경우
+    if d[nx][ny] == 0 or array[nx][ny] == 0:
+        d[nx][ny] = 1
+        x = nx
+        y = ny
+        count += 1
+        turn_time = 0
+        continue 
+    # 회전한 이후 정면에 가보지 않은 칸이 없거나 바다인 경우
+    else:
+        turn_time += 1
+    # 네 방향 모두 갈 수 없는 경우
+    if turn_time == 4:
+        nx = x - dx[direction]
+        ny = y - dy[direction]
+        # 뒤로 갈 수 있다면 이동하기
+        if array[nx][ny] == 0:
             x = nx
-            y = ny
-            count += 1
-            turn_time = 0
-            continue
-        
-        #회전한 이후 정명에 가보지 않은 칸이 없거나 바다인 경우
+            y = ny 
         else:
-            turn_time += 1
-        # 네 방향 모두 갈 수 없는 경우
-        if turn_time == 4:
-            nx = x - dx[direction]
-            ny = y - dy[direction]
-            #뒤로 갈 수 있다면 이동하기
-            if array[nx][ny] == 0:
-                x = nx
-                y = ny 
-            # 뒤가 바다로 막혀이쓴 경우
-            else:
-                break
-            turn_time = 0
-    print(count)
+            break 
+        turn_time - 0
+print(count)
