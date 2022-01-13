4518f232fb314fbcafd8de54af0ea265473b4ea6
sujeongHeo401 <gotnwjd40@gmail.com>
2022-01-14 04:41:25 +0900

    에러 수정

--- bak_11000/sol_real.py
+++ bak_11000/sol_real.py
@@ -7,7 +7,7 @@ h = []
 for i in range(N):
     C.append(list(map(int, sys.stdin.readline().split())))
 
-C.sorted(C, key = lambda x : x[0])
+C =C.sorted(C, key = lambda x : x[0])
 for i in range(N):
     if len(h) != 0 and h[0] <= C[i][0]:
         heapq.heappop(h)
