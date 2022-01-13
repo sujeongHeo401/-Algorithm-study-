46eed0b05f834505214f2fff3107719caaec66dd
sujeongHeo401 <gotnwjd40@gmail.com>
2022-01-13 23:25:17 +0900

    4-4 번 다시 풂

--- python-for-cote/4/4-4 copy.py
+++ python-for-cote/4/4-4 copy.py
@@ -1,44 +1,44 @@
-# n, m 을 공백으로 구분하여 입력받기
+# N, M을 공백을 기준으로 구분하여 입력받기
 n, m = map(int, input().split())
-print("n : ", n)
-#방문한 위치를 저장히기 위한 맵을 생성하여 0으로 초기화
-d =  [[0]  * m for _ in range(n)]
-# 현재 캐릭터의 x 좌표, y 좌표, 방향을 입력받기
+
+# 방문한 위치를 저장하기 위한 맵을 생성하여 0으로 초기화
+d = [[0] * m for _ in range(n)]
+# 현재 캐릭터의 X 좌표, Y 좌표, 방향을 입력받기
 x, y, direction = map(int, input().split())
-d[x][y] =1 # 현재 좌표 방문 처리
+d[x][y] = 1 # 현재 좌표 방문 처리
 
-#전체 맵 정보 보기 
-array = [] 
-
+# 전체 맵 정보를 입력받기
+array = []
 for i in range(n):
     array.append(list(map(int, input().split())))
 
-# 북 ,동, 남, 서 방향 정의 
+# 북, 동, 남, 서 방향 정의
 dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
 
-#왼쪽으로 회전
+# 왼쪽으로 회전
 def turn_left():
     global direction
     direction -= 1
     if direction == -1:
         direction = 3
 
-# 시뮬레이션 시작 
+# 시뮬레이션 시작
 count = 1
 turn_time = 0
 while True:
+    # 왼쪽으로 회전
     turn_left()
     nx = x + dx[direction]
     ny = y + dy[direction]
-    # 회전한 이후 정면에 가보지 않은 칸이 존재하는 경우
-    if d[nx][ny] == 0 or array[nx][ny] == 0:
+    # 회전한 이후 정면에 가보지 않은 칸이 존재하는 경우 이동
+    if d[nx][ny] == 0 and array[nx][ny] == 0:
         d[nx][ny] = 1
         x = nx
         y = ny
         count += 1
         turn_time = 0
-        continue 
+        continue
     # 회전한 이후 정면에 가보지 않은 칸이 없거나 바다인 경우
     else:
         turn_time += 1
@@ -49,8 +49,11 @@ while True:
         # 뒤로 갈 수 있다면 이동하기
         if array[nx][ny] == 0:
             x = nx
-            y = ny 
+            y = ny
+        # 뒤가 바다로 막혀있는 경우
         else:
-            break 
+            break
         turn_time = 0
+
+# 정답 출력
 print(count)
