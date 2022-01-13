85dea0ba9d53f24c228dda33031db34a9dbb20a3
sujeongHeo401 <gotnwjd40@gmail.com>
2022-01-14 04:30:38 +0900

    c++ 버전도

---
+++ bak_11000/sol.cpp
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+vector <pair<int, int>> v;
+
+bool startTime(const pair<int, int> &a, const pair<int, int> &b){
+    if(a.first == b.first){
+        return a.second < b.second;
+    }
+    return a.first< b.first;
+}
+
+struct compare {
+    bool operator()(const pair<int, int> &a, const pair<int, int> &b){
+        if (a.second == b.second){
+            // 끝나는 시간이 같으면 시작 시간이 빠른 순 
+            return a.first > b.first;
+        }
+        // 끝나는 시간이 빠른 순 
+        return a.second > b.second;
+    }
+};
+
+priority_queue<pair<int, int>, vector<pair<int, int>>, compare> pq;
+
+int find_room_count(int n){
+    int cnt = 1;
+    sort(v.begin(), v.end(), startTime);
+    // 정렬된 첫번째 원소 삽입 (시작 시간 빠른 순)
+    pq.push(v.at(0));
+
+    for (int i = 1; i < n; i++){
+        if(pq.top().second > v.at(i).first){
+            pq.push(v.at(i));
+            cnt++;
+        } else{
+            pq.pop();
+            pq.push(v.at(i));
+        }
+    }
+    return cnt;
+}
+
+int main(){
+    int n; 
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++){
+        int start, end;
+        scanf("%d %d", &start, &end);
+        v.push_back(make_pair(start, end));
+    }
+
+    int count = find_room_count(n);
+    printf("%d", count);
+
+    return 0;
+}
--- bak_11000/sol_again.py
+++ bak_11000/sol_again.py
@@ -3,13 +3,11 @@ n = int(input()) # 입력
 arr = []
 for i in range(n):
     arr.append(tuple(map(int, input().split())))####### 배열 입력
+arr.sort()
 room = []
 heapq.heappush(room, arr[0][1])
-
-for i in range(1, len(arr)):
-    if room[0] > arr[i][0]: # 빨리 시작하면 
-        heapq.heappush(room, arr[i][1])
-    else:
-        heapq.heappop()
-        heapq.heappush(room, arr[i][1])
-print("len(room) : ", len(room))
+for i in range(1, n):
+    if room and room[0] <= arr[i][0]:
+        heapq.heappop(room)
+    heapq.heappush(room, arr[i][1])
+print(len(room))
